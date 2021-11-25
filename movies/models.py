b08b3790e2d7fe0bb940e427c44e78732968e0d2
Max Shchur <mxm.shchur@gmail.com>
2021-11-25 16:47:42 +0200

    Added file that inserts movies.Also Movie and Actor were created

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=140)
+    year = models.PositiveIntegerField(default=2000)
+    runtime = models.PositiveIntegerField(default=0)
+    rating = models.FloatField(default=0.0)
+    actors = models.CharField(max_length=300, default='')
+
+    #objects = MovieManager()
+    def __str__(self):
+        return "{} ({})year {}hr {}stars/10.\nActors: {}.".format(self.name, self.year, self.runtime, self.rating, self.actors)
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=140)
+    age = models.PositiveIntegerField(default=20)
+
+    def __str__(self):
+        return "{}.{}years old".format(self.name, self.age)
+
+
+# class Role(models.Model):
+#     name = models.CharField(max_length=140)
+#
+#
+#     #objects = MovieManager()
+#     def __str__(self):
+#         return "{}".format(self.name)
